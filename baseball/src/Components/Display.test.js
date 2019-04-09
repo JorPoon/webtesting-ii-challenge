d2c1a7951d2338b26f6dbbc104141d71c17d04ca
Jor_Poon <jormpoon@gmail.com>
2019-04-09 18:36:17 -0400

    testing display component

--- baseball/src/Components/Display.test.js
+++ baseball/src/Components/Display.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {render} from 'react-testing-library';
+import {render, cleanup, fireEvent} from 'react-testing-library';
 import 'jest-dom/extend-expect';
 import Display from './Display'
+
+// const here = afterEach(cleanup());
+// here();
 
 describe('<Display/>', () => {
 
@@ -13,5 +16,26 @@ describe('<Display/>', () => {
   it('renders strikes', () => {
       const {queryByText} = render(<Display/>)
       expect(queryByText(/strikes/i)).not.toBeNull();
+  })
+
+  it('renders balls', () => {
+    const {queryByText} = render(<Display/>)
+    expect(queryByText(/balls/i)).not.toBeNull();
+  })
+
+  it('renders hits', () => {
+    const {queryByText} = render(<Display/>)
+    expect(queryByText(/hits/i)).not.toBeNull();
+  })
+
+  it('resets all counts', () => {
+    const {getByText} = render(<Display/>);
+    const button = getByText(/reset/i)
+    fireEvent.click(button);
+    getByText(/strikes/i);
+    // getByText(/balls: 0/i);
+    // getByText(/hits: 0/i);
+
+
   })
 })
